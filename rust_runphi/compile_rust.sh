d42ba5d81fa3e405972ff0ad9c097cc18b8f12eb
root <marco.barletta@unina.it>
2025-02-11 12:13:36 +0100

    fix compile_rust since compilation must run within a container

--- rust_runphi/compile_rust.sh
+++ rust_runphi/compile_rust.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-cargo build --release --target aarch64-unknown-linux-gnu
+
+# build image for building rust runphi
+docker build -t rust_runphi_builder .
+
+# compile rust runphi
+docker run -it --rm --name compile_rust_runphi -v ${PWD}:/home -w="/home" rust_runphi_builder cargo build --release --target aarch64-unknown-linux-gnu
