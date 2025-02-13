49503cb8f9ca6a68cb2277f1e46115f7a15d6804
Francesco Boccola <francesco.boccola@unina.it>
2025-02-13 13:20:36 +0100

    changed the address of the ivshmem for zcu and kria and slight change to the container start script to cd in /home directly

--- docker/start_container.sh
+++ docker/start_container.sh
@@ -3,6 +3,6 @@ docker build -t runphi_builder .
 docker stop runphi_manager_compiler
 docker rm runphi_manager_compiler
 cd .. 
-docker run -it -v ./rust_runphi:/home --name runphi_manager_compiler runphi_builder /bin/bash
+docker run -it -v ./rust_runphi:/home -w="/home" --name runphi_manager_compiler runphi_builder /bin/bash
 
 
